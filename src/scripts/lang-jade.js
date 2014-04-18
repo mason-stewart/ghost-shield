af875291202c74070e792a731d886bffc5383fec
Mason Stewart <masondesu@gmail.com>
2014-04-18 17:26:46 -0400

    Possible improvment to lexer when reading class and variable names with numbers

--- src/scripts/lang-jade.js
+++ src/scripts/lang-jade.js
@@ -18,7 +18,7 @@ PR['registerLangHandler'](
       // cheap trick... :(
       [PR['PR_TYPE'], /---/, null, '---'],
       
-      [PR['PR_KEYWORD'], /\$[a-zA-Z-]+/, null, '$'],
+      [PR['PR_KEYWORD'], /\$[\w\d-]+/, null, '$'],
 
       // elemets with class or id... .example or #example
       [PR['PR_ATTRIB_VALUE'], /[.#][a-zA-Z-]+/, null, '.#'],
@@ -36,7 +36,7 @@ PR['registerLangHandler'](
       // pipe string
       [PR['PR_STRING'], /^\s*\|/],
 
-      [PR['PR_ATTRIB_VALUE'], /[.#][a-zA-Z-]+/],
+      [PR['PR_ATTRIB_VALUE'], /[.#][\w\d-]+/],
       
       // tag
       [PR['PR_TAG'], /(\n[ ]*)(abbr|address|area|are|aside|audio|a|base|bdi|bdo|blockquote|body|body|br|button|b|canvas|caption|cite|code|colgroup|col|datalist|datalist|data|dd|dd|del|details|details|dfn|div|dl|d|embedobject|em|fieldset|fieldset|figcaption|figure|footer|form|h1|h2|h3|h4|h5|h6|header|head|hr|html|iframe|img|input|ins|i|kbd|keygen|label|lgend|link|li|main|map|map|mark|menumenuitem|meta|meter|navarticle|noscript|object|ol|optgroup|option|output|param|pre|progress|p|q|rp|rt|ruby|script|section|select|select|small|source|span|strong|style|sub|summary|sup|svg|s|table|tbody|td|template|tetarea|tfoot|thead|th|time|title|track|tr|ul|u|varsamp|video|video|video|wbr|)/],
