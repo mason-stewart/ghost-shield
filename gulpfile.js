948d4c5f9601a18f83b3cbb886a2ff077a0891bd
Mason Stewart <masondesu@gmail.com>
2014-05-04 20:48:52 -0400

    Don't include html in the dist EVAR

--- gulpfile.js
+++ gulpfile.js
@@ -62,7 +62,7 @@ gulp.task('templates', function() {
     .pipe(jade({
       pretty: true
     }))
-    .pipe(gulp.dest('dist/'))
+    .pipe(gulp.dest('.tmp/'))
 });
 
 gulp.task('express', function() {
